449837a5bd8530d11f76458a26e992a0afacfd7a
ben avrahami <ben.avrahami@biocatch.com>
2023-03-02 15:38:23 +0200

    vbump + attach openapi to release

---
+++ tools/generate_openapi.py
@@ -0,0 +1,12 @@
+import json
+import tomllib
+from heksher.main import app
+
+app = app()
+with open("pyproject.toml", "rb") as proj_file:
+    proj = tomllib.load(proj_file)
+    app.version = proj['tool']['poetry']['version']
+openapi = app.openapi()
+
+with open("openapi.json", "x") as file:
+    json.dump(openapi, file)
